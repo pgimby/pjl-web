affcb31bc909d793fa060401e29e20e0c72cc98f
wkyle <wrkyle@mail.com>
2017-07-06 11:03:31 -0600

    mobile fix update

--- js/pjl.js
+++ js/pjl.js
@@ -80,7 +80,6 @@ $(document).on("click", "select", function(e) {
 $(document).on("mousedown", "option", function(e) {
     e.preventDefault();
     $(this).prop('selected', !$(this).prop('selected'));
-    filterResults(getCurrentFilter());
     return false;
 });
 
