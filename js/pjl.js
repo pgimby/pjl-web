8ff21f3ab856838062e5278ba3aac3027dc085c3
wkyle <wrkyle@mail.com>
2017-08-04 14:21:00 -0600

    pushing work on database equipment entries

--- js/pjl.js
+++ js/pjl.js
@@ -4,10 +4,10 @@
 //   GLOBALS
 //*******************************************************************************************
 
-var mainxmlpath = "/data/labDB.xml";
+var mainxmlpath = "/dev/labDB.xml";
 var zipoutputfilename = "PJL-lab-docs.zip";
-var siteroot = "/pjl-web";
-//var siteroot = "";
+//var siteroot = "/pjl-web";
+var siteroot = "";
 
 
 // Do __NOT__ change classes or ids without checking jQuery and D3 selectors in the JS code
