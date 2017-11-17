1595a713d5f93e4d49c858c7cb2b61328baa995e
root <root@watt.uc.ucalgary.ca>
2017-11-17 14:07:33 -0700

    .

--- dev/bash-tools/webpageUpdate.sh
+++ dev/bash-tools/webpageUpdate.sh
@@ -11,19 +11,25 @@ RSYNC="/usr/bin/rsync"
 
 
 # Source paths
-webSource="/home/pgimby/pjl-web/"
-repositorySource="/usr/local/master/labs/repository/"
+#webSource="/home/pgimby/pjl-web/"
+#webSource="/mnt/local/repository.slug/"
+repositorySource="/mnt/local/repository.slug/"
 
 # Destinations
-webDest="watt:/mnt/local/legacy/html-future/"
-repositoryDest="watt:/mnt/local/repository/"
+#webDest="/mnt/local/legacy/html-future/"
+repositoryDest="/mnt/local/repository/"
 
+$RSYNC --delete -anvz $repositorySource $repositoryDest
+
+
+
+exit
 # set permissions
-chmod 644 $webSource/index.html
-chmod 644 $webSource/css/site-wide.css
-chmod 644 $webSource/js/pjl.js
-chmod 644 $webSource/js/jquery-3.2.1.min.js
-chmod 644 $webSource/img/*
+#chmod 644 $webSource/index.html
+#chmod 644 $webSource/css/site-wide.css
+#chmod 644 $webSource/js/pjl.js
+#chmod 644 $webSource/js/jquery-3.2.1.min.js
+#chmod 644 $webSource/img/*
 chmod 755 $webSource/repository
 chmod 644 $webSource/repository/index.html
 
@@ -33,7 +39,6 @@ chmod 644 $webSource/repository/index.html
 
 
 $RSYNC --delete -avz $webSource pgimby@$webDest
-$RSYNC --delete -avz $repositorySource pgimby@$repositoryDest
 
 
 #ls $webCode
