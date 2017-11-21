9a3e9366f2a7e371d1673078ddb82b0a88ae1f52
wkyle <wrkyle@mail.com>
2017-11-21 10:57:49 -0700

    .

--- js/pjl.js
+++ js/pjl.js
@@ -547,6 +547,7 @@ $(document).on("mouseleave", ".resource-dropdown", function(e) {
 
 
 
+
 $(document).on("click", ".resource-dropdown-content, .mobile-resource-dropdown-content", function(e) {
 	var links = {"pjl-regress": "/",
 				 "pjl-lab-schedule": "/",
