5a1ee59e445303ca5158243a4561f71c38d589fb
root <root@watt.uc.ucalgary.ca>
2017-11-23 13:48:28 -0700

    added folders for other documents to post

--- dev/python-tools/liveUpdate.py
+++ dev/python-tools/liveUpdate.py
@@ -11,6 +11,7 @@ import os
 # define folder locations
 slugFolder = "slug:/usr/local/master/labs"
 sourceFolder = "/mnt/local/labs.slug"
+destFolder = "/mnt/local/labs"
 repositoryFolder = "/mnt/local/labs/repository"
 webRoot = "/var/www/html"
 
@@ -24,26 +25,26 @@ os.system("mount " + slugFolder + " " + sourceFolder)
 
 
 # sync master copy to live copy
-os.system("rsync --delete -anvz " + sourceFolder + "/ " + destFolder + "/")
+os.system("rsync --delete -avz " + sourceFolder + "/ " + destFolder + "/")
 
 
 # unmount source files
-#os.system("umount " + sourceFolder)
+os.system("umount " + sourceFolder)
 
 
 # change permissions and ownerships of files and folders
-#os.system("find " + destFolder + " -type d -exec chmod 755 {} \;")
-#os.system("find " + destFolder + " -type f -exec chmod 644 {} \;")
-#os.system("find " + destFolder + " -type d -exec chown " + owner + "." + group + " {} \;")
-#os.system("find " + destFolder + " -type d -exec chown " + owner + "." + group + " {} \;")
+os.system("find " + destFolder + " -type d -exec chmod 755 {} \;")
+os.system("find " + destFolder + " -type f -exec chmod 644 {} \;")
+os.system("find " + destFolder + " -type d -exec chown " + owner + "." + group + " {} \;")
+os.system("find " + destFolder + " -type d -exec chown " + owner + "." + group + " {} \;")
 
-#os.system("chown root." + apacheUser + " " + webRoot + "/data/labDB.xml" )
-#os.system("chown root." + apacheUser + " " + webRoot + "/data/equipmentDB.xml" )
-#os.system("chmod 660 " + webRoot + "/data/labDB.xml" )
-#os.system("chmod 660 " + webRoot + "/data/equipmentDB.xml" )
+os.system("chown root." + apacheUser + " " + webRoot + "/data/labDB.xml" )
+os.system("chown root." + apacheUser + " " + webRoot + "/data/equipmentDB.xml" )
+os.system("chmod 660 " + webRoot + "/data/labDB.xml" )
+os.system("chmod 660 " + webRoot + "/data/equipmentDB.xml" )
 
-#os.system("chown root." + apacheUser + " " + webRoot + "/data" )
-#os.system("chmod 775 " + webRoot + "/data" )
+os.system("chown root." + apacheUser + " " + webRoot + "/data" )
+os.system("chmod 775 " + webRoot + "/data" )
 
 # confirm end of script
 print("...and then there will be cake")
