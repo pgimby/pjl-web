98b29e88b7ed2ae2e845af2fe09b5d2c12cde382
root <root@watt.uc.ucalgary.ca>
2017-11-23 14:11:40 -0700

    added lab scheduels

--- js/pjl.js
+++ js/pjl.js
@@ -549,6 +549,7 @@ $(document).on("mouseleave", ".resource-dropdown", function(e) {
 $(document).on("click", ".resource-dropdown-content, .mobile-resource-dropdown-content", function(e) {
 	var links = {"pjl-regress": "/",
 				 "pjl-lab-schedule": "/data/schedules/schedule-current.pdf",
+				 "pjl-rooms-schedule": "/data/schedules/rooms-current.pdf",
 				 "pjl-geiger": "/",
 				 "pjl-repository": "/repository",
 				 "pjl-linearization": "/",
