d709ac5906fecbe0ca43f01c1d4109cadfe7cccf
Peter Gimby <pgimby@ucalgary.ca>
2018-01-08 12:40:23 -0700

    added 497WI2018

--- dev/python-tools/addVersions.py
+++ dev/python-tools/addVersions.py
@@ -6,21 +6,23 @@ from pjlDB import *
 import csv
 
 
-new_versions = [['0102', '/data/repository/0102-AC-Circuits-and-Voltage-Dividers/0102-PHYS497WI2018/AC-Circuits-and-Voltage-Dividers-WI2018.pdf ', 'Winter', '2018', 'PHYS 497']]
+new_versions = [[0102, '/data/repository/0102-AC-Circuits-and-Voltage-Dividers/0102-PHYS497WI2018/AC-Circuits-and-Voltage-Dividers-WI2018.pdf ', 'Winter', '2018', 'PHYS 497']]
 
 db = LabDB("../../data/labDB.xml")
 
 for version in new_versions:
 
-	print(version)
+	#print(version)
 	lab = db.getLab(idnum=version[0])
-	new_version = {"path": version[1],
-      	        	"semester": version[2],
-           	    	"year": version[3],
-               		"course": version[4],
-               		"directory": "/" + "/".join(version[1].split("/")[:-1])}
-    
-	lab.addVersion(new_version)
-	db.addLab(lab)
+	print(version[0])
+	new_version = {"path": version[1],"semester": version[2],"year": version[3],"course": version[4],"directory": "/".join(version[1].split("/")[:-1])}
+	print(new_version)
+	#for i in new_version:
+	#	print(i)
 
-db.save("/dev/updatedlabDB.xml", ignore_validation=False, error_log=True)
+	#print(new_version)
+	lab.addVersion(new_version)
+	#db.addLab(lab)
+
+#db.save("/dev/updatedlabDB.xml", ignore_validation=False, error_log=True)
+
