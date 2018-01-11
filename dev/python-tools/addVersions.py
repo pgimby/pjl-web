47cdfc0ef31bbb59af3d3cee482db6d5e5bb6a69
Peter Gimby <pgimby@ucalgary.ca>
2018-01-11 14:30:17 -0700

    almost working addVersions script

--- dev/python-tools/addVersions.py
+++ dev/python-tools/addVersions.py
@@ -6,23 +6,19 @@ from pjlDB import *
 import csv
 
 
-new_versions = [[0102, '/data/repository/0102-AC-Circuits-and-Voltage-Dividers/0102-PHYS497WI2018/AC-Circuits-and-Voltage-Dividers-WI2018.pdf ', 'Winter', '2018', 'PHYS 497']]
+with open("lst.csv", 'r') as o:
+	reader = csv.reader(o)
+	new_versions = list(reader)
+#new_versions = [['0102', '/data/repository/0102-AC-Circuits-and-Voltage-Dividers/0102-PHYS497WI2018/AC-Circuits-and-Voltage-Dividers-WI2018.pdf ', 'Winter', '2018', 'PHYS 497']]
 
-db = LabDB("../../data/labDB.xml")
+	db = LabDB("../../data/labDB.xml")
 
-for version in new_versions:
+	for version in new_versions:
+		lab = db.getLab(idnum=version[0])
+		new_version = {"path": version[1],"semester": version[2],"year": version[3],"course": version[4],"directory": "/".join(version[1].split("/")[:-1])}
 
-	#print(version)
-	lab = db.getLab(idnum=version[0])
-	print(version[0])
-	new_version = {"path": version[1],"semester": version[2],"year": version[3],"course": version[4],"directory": "/".join(version[1].split("/")[:-1])}
-	print(new_version)
-	#for i in new_version:
-	#	print(i)
-
-	#print(new_version)
 	lab.addVersion(new_version)
-	#db.addLab(lab)
+	db.addLab(lab)
 
-#db.save("/dev/updatedlabDB.xml", ignore_validation=False, error_log=True)
-
+#db.save("/dev/tmp.xml", ignore_validation=True, error_log=True)
+db.save("/dev/updatedlabDB.xml", ignore_validation=False, error_log=True)
